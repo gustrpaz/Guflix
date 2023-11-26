609c33d146d765dcfbd5082dc478b36ea1a04987
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2023-11-26 15:01:04 -0300

    MVP funcionando

--- Frontend/guflix/src/app/filme/page.tsx
+++ Frontend/guflix/src/app/filme/page.tsx
@@ -5,14 +5,17 @@ import Image from 'next/image'
 import Trash from "../../assets/Trash.png"
 interface Filme {
   idFilme: number,
-  Genero: number,
+  genero: string,
   nomeFilme: string
 }
 export default function Filme() {
   const [filmes, setFilmes] = useState<Filme[]>([]);
   const [nomeFilme, setNomeFilme] = useState('');
+  const [novoNomeFilme, setNovoNomeFilme] = useState('');
   const [nomeGenero, setNomeGenero] = useState('');
-  const [filmeSelecionado, setFilmeSelecionado] = useState(null);
+  const [novoNomeGenero, setNovoNomeGenero] = useState('');
+  const [filmeSelecionado, setFilmeSelecionado] = useState<number | undefined>(undefined);
+
   async function ListarFilmes() {
     try {
       const Resposta = await api.get('/Filmes');
@@ -36,23 +39,20 @@ export default function Filme() {
         nomeFilme: nomeFilme,
         genero: nomeGenero,
       };
-
       await api.post('/Filmes', FilmeData);
-      console.log('Filme cadastrado com sucesso!');
       ListarFilmes();
     }
     catch (erro) {
       console.error('Erro ao fazer requisição', erro);
     }
   }
-  async function AtualizarFilme(idFilme: number) {
+  async function AtualizarFilme(idFilme: number, nomeFilme: string, nomeGenero: string) {
     try {
       const FilmeAtualizado = {
         nomeFilme: nomeFilme,
+        nomeGenero: nomeGenero,
       };
-
       await api.put(`/Filmes/${idFilme}`, FilmeAtualizado);
-      console.log('Filme atualizado com sucesso!')
       ListarFilmes();
     }
     catch (erro) {
@@ -63,67 +63,85 @@ export default function Filme() {
     ListarFilmes();
   }, []);
   return (
-    <main>
+    <>
+      <input
+        type="text"
+        placeholder="Digite o nome"
+        value={nomeFilme}
+        onChange={(f) => setNomeFilme(f.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite o gênero"
+        value={nomeGenero}
+        onChange={(f) => setNomeGenero(f.target.value)}
+      />
+      <button onClick={() => CadastrarFilmes(nomeFilme, nomeGenero)}>Cadastrar</button>
       <table>
         <thead>
           <tr>
-            <th>Cadastrar um Filme</th>
+            <th colSpan={2}>FILMES</th>
+          </tr>
+          <tr>
+            <th>Nome</th>
+            <th>Gênero</th>
+            <th>Ações</th>
           </tr>
         </thead>
-
         <tbody>
+          {filmes.map((filme) => (
+            <tr key={filme.idFilme}>
+              <td>{filme.nomeFilme}</td>
+              <td>{filme.genero}</td>
+              <td>
+                <button className='btn' onClick={() => DeletarFilme(filme.idFilme)}>
+                  <Image src={Trash} width={21} height={21} alt='Apagar' />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <form>
+        <div>
+          <select
+            name="filme"
+            id="filme"
+            value={filmeSelecionado !== undefined ? String(filmeSelecionado) : ''}
+            onChange={(e) => setFilmeSelecionado(e.target.value !== '' ? Number(e.target.value) : undefined)}
+          >
+            <option value="">Selecione o filme</option>
+            {filmes.map((f) => (
+              <option key={f.idFilme} value={f.idFilme}>
+                {f.nomeFilme}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
           <input
             type="text"
-            placeholder="Digite o nome"
-            value={nomeFilme}
-            onChange={(f) => setNomeFilme(f.target.value)}
-          />
-          <button onClick={() => CadastrarFilmes(nomeFilme, nomeGenero)}>Cadastrar</button>
-        </tbody>
-      </table>
-      <table>
-        <thead>
-          <tr>
-            <th>Filmes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filmes
-            .map((filme) => (
-              <tr key={filme.idFilme}>
-                <td >{filme.nomeFilme}</td>
-                <td >
-                  <button className='btn' onClick={() => DeletarFilme(filme.idFilme)}>
-                    <Image src={Trash} width={21} height={21} alt='Apagar' />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          <form onSubmit={AtualizarFilme}>
-            <div>
-              <label htmlFor="filmeSelect">Selecione um filme:</label>
-              <select 
-              id="filme" 
-              value={filmeSelecionado} 
-              onChange={() => setFilmeSelecionado(g.target.value)}>
-                <option value="" selected>Selecione um gênero</option>
-              {filmes.map((filme) => (
-                  <option key={filme.idFilme} value={filme.idFilme}>
-                    {filme.nomeFilme}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <label htmlFor="textInput">Novo nome Filme</label>
-              <input type="text" id="textInput" placeholder="Ex Avatar"></input>
-            </div>
-            <div>
-              <button type="submit">ATUALIZAR</button>
-            </div>
-          </form>
-        </tbody>
-      </table>
-    </main>
+            id="filmeInput"
+            placeholder="Novo filme"
+            value={novoNomeFilme}
+            onChange={(e) => setNovoNomeFilme(e.target.value)}></input>
+        </div>
+        <div>
+          <input
+            type="text"
+            id="generoInput"
+            placeholder="Novo Gênero"
+            value={novoNomeGenero}
+            onChange={(e) => setNovoNomeGenero(e.target.value)}></input>
+        </div>
+        <div>
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              filmeSelecionado !== undefined && AtualizarFilme(filmeSelecionado, novoNomeFilme, novoNomeGenero)
+            }}>ATUALIZAR</button>
+        </div>
+      </form>
+    </>
   )
 }
--- Frontend/guflix/src/app/login/page.tsx
+++ Frontend/guflix/src/app/login/page.tsx
@@ -2,42 +2,45 @@
 import '../page.css'
 import Link from 'next/link'
 import { api } from '@/lib/api';
+import { useRouter } from 'next/navigation'
+
 interface User {
   email: string;
   password: string;
 }
-async function logar(event: React.FormEvent<HTMLFormElement>) {
-  event.preventDefault();
-  const email = (event.target as any).email.value;
-  const password = (event.target as any).password.value;
-  try {
-    const user: User = {
-      email,
-      password
-    };
-    const response = await api.post('/login', user);
-    console.log(`token= ${response.data.token}`)
-  } catch (error) {
-    console.error('Erro ao fazer a requisição:', error);
-  }
-}
 export default function Login() {
+  async function Login(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const email = (event.target as any).email.value;
+    const password = (event.target as any).password.value;
+    try {
+      const user: User = {
+        email,
+        password
+      };
+      const response = await api.post('/login', user);
+      console.log(`token= ${response.data.token}`)
+      router.push('/filme')
+    } catch (error) {
+      alert('Senha incorreta!')
+      console.error('Erro ao fazer a requisição:', error);
+    }
+  }
+  const router = useRouter()
   return (
     <main>
-      <form onSubmit={logar}>
+      <form onSubmit={Login}>
         <div className="Form">
           <div>
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" name="email"/>
+            <input type="email" id="email" name="email" />
           </div>
           <div>
             <label htmlFor="password">Senha</label>
             <input type="password" id="password" name="password" />
           </div>
           <div>
-            <Link href="/filme">
-              <button type="submit">LOGIN</button>
-            </Link>
+            <button type="submit">LOGIN</button>
           </div>
         </div>
       </form>
